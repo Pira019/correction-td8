944924ea94fdfa7e979fb557a43490df72d896c8
pires lufungula <pires00243@gmail.com>
2024-04-02 23:53:52 -0400

    question2

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
+from typing import Callable,List
 def multiple(m, n):
     if m < n:
         return False
     elif m == n:
         return True
-    print(m - n)
+    #print(m - n)
     return multiple(m - n, n)
 
 # Exemples
-print(multiple(10, 2))  #  True
+#print(multiple(10, 2))  #  True
 '''print(multiple(7, 3))   #  False 
 print(multiple(15, 3))  # TRUE'''
  
+
+def diviseursR(m, n):
+    if n > m / 2:
+        return []
+    elif multiple(m,n) : #m % n == 0
+        return [n] + diviseursR(m, n + 1) 
+    return diviseursR(m, n + 1)
+ 
+diviseurs: Callable[[int], List[int]] = lambda m: diviseursR(m, 1) 
+print(diviseurs(10))  # ex :12 [1, 2, 3, 4, 6]
