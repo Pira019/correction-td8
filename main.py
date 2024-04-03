866eeb7605068eb1b6f0af35ef8c3fcf3714ef9a
pires lufungula <pires00243@gmail.com>
2024-04-02 23:20:35 -0400

    Q1 reponse

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print(10)
+def multiple(m, n):
+    if m < n:
+        return False
+    elif m == n:
+        return True
+    print(m - n)
+    return multiple(m - n, n)
+
+# Exemples
+print(multiple(10, 2))  #  True
+'''print(multiple(7, 3))   #  False 
+print(multiple(15, 3))  # TRUE'''
+ 
